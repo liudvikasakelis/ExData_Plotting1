347504511d7b8159332741dcbf6d1e3c56838466
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-11-12 23:45:01 +0200

    the first plot

--- plot1.R
+++ plot1.R
@@ -1,1 +1,19 @@
-edata <- read.csv('household_power_consumption.txt', delim=';', header=T)
+### import the data 
+
+edata <- read.csv('household_power_consumption.txt', sep=';', header=T, 
+                  stringsAsFactors = F)
+edata <- edata[edata$Date %in% c('1/2/2007', '2/2/2007'),]
+edata$Global_active_power <- as.numeric(edata$Global_active_power)
+
+### Draw the plot
+
+png('plot1.png', width=480, height=480, bg='transparent')
+
+hist(edata$Global_active_power, 
+     main="Global Active Power",
+     col='red',
+     xlab='Global Reactive Power (kWh)')
+
+
+
+dev.off()
